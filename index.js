910dd8140482f6d72fd737a357c43254c9696db6
zeek-r <bistaroshan03@gmail.com>
2020-04-09 23:29:19 +0545

    Added new cli options and logs

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ const parser = {
   "--opts": (command) => {
     return { opts: command.split("=")[1] };
   },
+  "db": () => {
+    return { db: true };
+  },
+  "s3": () => {
+    return { s3: true };
+  }
 };
 
 (async () => {
@@ -31,17 +37,24 @@ const parser = {
     }
   }
   try {
-    await db.init();
     console.log("db connection initialized");
     let directory = converterCommands.output.split("/");
     directory = directory.splice(0, directory.length - 1).join("/");
     await mkdir(directory, { recursive: true });
     console.log(`Directory created ${directory}`)
     await converter(converterCommands);
-    console.log("returning");
-    // console.log("conversions done");
-    await pushS3Directory(directory);
-    await storeUrl({ url: converterCommands.output });
+    console.log("Encodings done");
+
+    if (cliOps["s3"]) {
+      console.log("pushing to s3");
+      await pushS3Directory(directory);
+    }
+
+    if (cliOps["db"]) {
+      console.log("Saving to db");
+      await db.init();
+      await storeUrl({ url: converterCommands.output });
+    }
   } catch (error) {
     console.error(error);
     return;
